a4f99b932a3a9d8bbacbdc16beb4320701e3e0a5
jeanloupPecquais <jeanlouppecquais@ik.me>
2023-02-09 18:17:54 +0100

    reorganize project. Continue master script

--- TEST/test.py
+++ TEST/test.py
@@ -1,22 +1,59 @@
-import yaml
 import os
+import yaml
+
+# Global Variables
+
+build_dir = "_build"
 
 
 # Get list of doc folder
 
-def getDocFolderList():
-    result = []
+def getFolderDepth(path):
+    steps = path.split(os.sep)
+    return len(steps)-1 # -1 to exclude ""./""
+
+def generateBuildPath(projectPath, buildPath):
+
+    n = getFolderDepth(projectPath)
     i = 0
-    for elem in os.listdir("."):
-        if os.path.isdir(elem) and elem[0] != "." and elem != "_build":
-            result.append(elem)
-            i += 1
+
+    while i < n:
+        buildPath = '../' + buildPath
+        i += 1
+
+    return buildPath
+
+
+def getProjectDict():
+
+# Return a dictionnary containing the name and the folder path of the quarto projects
+
+    result = {}
+
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file == "_quarto.yml":
+
+                name = ""
+                with open(os.path.join(root,file)) as fp:
+                    data = yaml.safe_load(fp)
+
+                if 'book' in data:
+                    #print(data['book']['title'])
+                    name = data['book']['title']
+                elif 'website' in data:
+                    #print(data['website']['title'])
+                    name = data['website']['title']
+
+                result.update({name: root})
+
     return result
 
 
 ##############################
 ##############################
 
-docList = getDocFolderList()
 
-print(docList)
+docDict = getProjectDict()
+
+print(docDict)
