49ad183b11448c5ef41ca6c32acbab36408ce24c
Jean Loup Pecquais <jeanlouppecquais@ik.me>
2023-02-21 16:35:48 +0100

    clean repo. Reroot build folder

---
+++ en_US/giftopng.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageDraw, ImageFont #dynamic import
+
+
+import os
+rootdir = './'
+extensions = ('.gif')
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        ext = os.path.splitext(file)[-1].lower()
+        if ext in extensions:
+            print (os.path.join(subdir, file))
+
+            gif=os.path.join(subdir, file)
+            img = Image.open(gif)
+            img.save(gif+".png",'png', optimize=True, quality=100)
